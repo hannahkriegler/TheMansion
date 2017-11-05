60aedb4b9517a344b7c48c61d45bc35ba1c21ab3
Niklas <NiklasBraun93@googlemail.com>
2017-11-05 21:54:06 +0100

    yow ein Testding

---
+++ Test_Mad_Flashlight_and_Sound/Assets/Look.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Look : MonoBehaviour {
+   
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    public Light flashlight;
+
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            flashlight.enabled = !flashlight.enabled;
+        }
+
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+        Vector3 v = new Vector3(pitch, yaw, 0.0f);
+        transform.eulerAngles = v;
+
+
+        
+	}
+}
---
+++ Test_Mad_Flashlight_and_Sound/Assets/Move.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+    public Camera cam;
+    public Light Flashlight;
+    public float speed = 0.05f;
+    private bool seen = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!seen)
+        {
+            transform.Translate(0, 0, 0.05f);
+        }
+
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit) && Flashlight.enabled)
+        {
+            if (hit.collider.tag == "Ghost")
+            {
+                seen = true;
+            }
+        } else
+        {
+            seen = false;
+        }
+
+
+
+    } 
+
+
+    void OnCollisionEnter(Collision collider)
+    {
+        GameObject target = collider.gameObject;
+        if (target.tag.Equals("Wall"))
+        {
+
+            transform.Rotate(0, 90, 0);
+        }
+        
+    }
+   
+    
+    
+}
