c231fd41341da95ffc09275e947b07fb332f843f
Hannah Kriegler <hannah.kriegler@gmail.com>
2017-11-12 23:59:29 +0100

    added 2 screens

---
+++ Prototypen/TheMansion/Assets/SimpleMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour {
+
+    public float speed = 5f;
+    private Rigidbody body;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var val = Input.GetAxis("Horizontal");
+        body.velocity = new Vector3(speed * val, body.velocity.y);
+	}
+}
