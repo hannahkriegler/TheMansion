d64cc9a8df1b300eb8727db1a736e100fefe46f6
Hannah Kriegler <hannah.kriegler@gmail.com>
2017-11-13 20:16:12 +0100

    two working chars

---
+++ Prototypen/TheMansion/Assets/Scripts/InputState.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ButtonState
+{
+    public bool value;
+    public float holdTime = 0;
+}
+
+public class InputState : MonoBehaviour {
+
+    private Dictionary<Buttons, ButtonState> buttonStates = new Dictionary<Buttons, ButtonState>();
+
+    public void SetButtonValue(Buttons key, bool value)
+    {
+        if (!buttonStates.ContainsKey(key))
+            buttonStates.Add(key, new ButtonState());
+
+        var state = buttonStates[key];
+
+        if(state.value && !value)
+        {
+            //Debug.Log("Button " + key + " released" + state.holdTime);
+            state.holdTime = 0;
+        } else if(state.value && value)
+        {
+            state.holdTime += Time.deltaTime;
+            //Debug.Log("Button " + key + " down" + state.holdTime);
+
+        }
+
+        state.value = value;
+    }
+
+    public bool GetButtonValue(Buttons key)
+    {
+        if (buttonStates.ContainsKey(key))
+            return buttonStates[key].value;
+        else
+            return false;
+    }
+}
---
+++ Prototypen/TheMansion/Assets/Scripts/SimpleMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour {
+
+    public float speed = 5f;
+    public Buttons[] input;
+    private Rigidbody body;
+    private InputState inputState;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody>();
+        inputState = GetComponent<InputState>();
+	}
+
+    // Update is called once per frame
+    void Update() {
+        var right = inputState.GetButtonValue(input[0]);
+        var left = inputState.GetButtonValue(input[1]);
+        var up = inputState.GetButtonValue(input[2]);
+        var down = inputState.GetButtonValue(input[3]);
+
+        Debug.Log("right is " + right);
+        Debug.Log("left is " + left);
+        Debug.Log("up is " + up);
+        Debug.Log("down is " + down);
+
+        var velX = speed;
+        var velZ = speed;
+
+        if ((right || left) && (up || down))
+        {
+            velZ *= down ? -1 : 1;
+            velX *= left ? -1 : 1;
+            Debug.Log("beide richtungen");
+        }
+        else if ((right || left) && !(up || down))
+        {
+            velX *= left ? -1 : 1;
+            velZ = 0;
+            Debug.Log("horizontal");
+
+        }
+        else if ((up || down) && (!right && !left))
+        {
+            velZ *= down ? -1 : 1;
+            velX = 0;
+            Debug.Log("vertical");
+
+        }
+        else
+        {
+            velX = 0;
+            velZ = 0;
+            Debug.Log("nothing");
+
+        }
+        body.velocity = new Vector3(velX, body.velocity.y, velZ);
+	}
+}
