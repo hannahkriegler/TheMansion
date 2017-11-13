26cbf6910236e42cbc65ca204556548c91aa565d
Hannah Kriegler <hannah.kriegler@gmail.com>
2017-11-13 20:35:29 +0100

    added niklas script, does not work yet

---
+++ Prototypen/TheMansion/Assets/Scripts/Hitreg.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hitregistration : MonoBehaviour
+{
+
+    /* Zur Nutzung dieses Scripts: 
+     * Hier wird davon ausgegangen, dass der Name des Scripts in den Geistern "Move" und die Bewegungsflag "seen" heißt. Des weiteren gehe ich davon aus, dass jeder Geist den Tag "Ghost" besitzt.
+     * Falls dem nicht so ist sollte dies im Code angepasst werden.
+     */
+    // Hierbei handelt es sich um die Taschenlampe des Spielers. Da es eine public Variable ist bitte in Unity reinziehen.
+    public Light flashlight;
+    private GameObject[] targets;
+    private ArrayList scripts;
+
+    // Use this for initialization
+    void Start()
+    {
+        scripts = new ArrayList();
+        targets = GameObject.FindGameObjectsWithTag("Ghost");
+        foreach (GameObject o in targets)
+        {
+            scripts.Add(o.GetComponent<SimpleMovement>());
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Falls an keiner weiteren Stelle die Taschenlampe an- bzw ausgestellt werden kann, kann das folgende Codesegment wieder aktiviert werden.
+
+        
+       if (Input.GetMouseButtonDown(0))
+       {   // aktiviert, bzw deaktiviert die Taschenlampe per linksklick.
+           flashlight.enabled = !flashlight.enabled;
+       }
+       
+
+        RaycastHit hit;
+        if (Physics.Raycast(flashlight.transform.position, flashlight.transform.forward, out hit) && flashlight.enabled)
+        {
+            if (hit.collider.tag == "Ghost")
+            {
+                SimpleMovement movementScript = hit.collider.GetComponent<SimpleMovement>();
+
+                movementScript.seen = true;
+
+            }
+            else
+            {
+                foreach (SimpleMovement o in scripts)
+                {
+                    o.seen = false;
+                }
+            }
+        }
+        else
+        {
+            foreach (SimpleMovement o in scripts)
+            {
+                o.seen = false;
+            }
+        }
+    }
+}
--- Prototypen/TheMansion/Assets/Scripts/SimpleMovement.cs
+++ Prototypen/TheMansion/Assets/Scripts/SimpleMovement.cs
@@ -6,6 +6,7 @@ public class SimpleMovement : MonoBehaviour {
 
     public float speed = 5f;
     public Buttons[] input;
+    public bool seen;
     private Rigidbody body;
     private InputState inputState;
 
@@ -17,46 +18,49 @@ public class SimpleMovement : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        var right = inputState.GetButtonValue(input[0]);
-        var left = inputState.GetButtonValue(input[1]);
-        var up = inputState.GetButtonValue(input[2]);
-        var down = inputState.GetButtonValue(input[3]);
-
-        Debug.Log("right is " + right);
-        Debug.Log("left is " + left);
-        Debug.Log("up is " + up);
-        Debug.Log("down is " + down);
-
-        var velX = speed;
-        var velZ = speed;
-
-        if ((right || left) && (up || down))
+        if (!seen)
         {
-            velZ *= down ? -1 : 1;
-            velX *= left ? -1 : 1;
-            Debug.Log("beide richtungen");
-        }
-        else if ((right || left) && !(up || down))
-        {
-            velX *= left ? -1 : 1;
-            velZ = 0;
-            Debug.Log("horizontal");
+            var right = inputState.GetButtonValue(input[0]);
+            var left = inputState.GetButtonValue(input[1]);
+            var up = inputState.GetButtonValue(input[2]);
+            var down = inputState.GetButtonValue(input[3]);
 
-        }
-        else if ((up || down) && (!right && !left))
-        {
-            velZ *= down ? -1 : 1;
-            velX = 0;
-            Debug.Log("vertical");
+            Debug.Log("right is " + right);
+            Debug.Log("left is " + left);
+            Debug.Log("up is " + up);
+            Debug.Log("down is " + down);
 
-        }
-        else
-        {
-            velX = 0;
-            velZ = 0;
-            Debug.Log("nothing");
+            var velX = speed;
+            var velZ = speed;
 
+            if ((right || left) && (up || down))
+            {
+                velZ *= down ? -1 : 1;
+                velX *= left ? -1 : 1;
+                Debug.Log("beide richtungen");
+            }
+            else if ((right || left) && !(up || down))
+            {
+                velX *= left ? -1 : 1;
+                velZ = 0;
+                Debug.Log("horizontal");
+
+            }
+            else if ((up || down) && (!right && !left))
+            {
+                velZ *= down ? -1 : 1;
+                velX = 0;
+                Debug.Log("vertical");
+
+            }
+            else
+            {
+                velX = 0;
+                velZ = 0;
+                Debug.Log("nothing");
+
+            }
+            body.velocity = new Vector3(velX, body.velocity.y, velZ);
         }
-        body.velocity = new Vector3(velX, body.velocity.y, velZ);
 	}
 }
