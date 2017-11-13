a35d56d178b9848986cbc0a89d5dc51b6294e66c
Niklas <NiklasBraun93@googlemail.com>
2017-11-13 18:10:36 +0100

    Neues Script Hitregistration mit auskommentierten Anweisungen. Damit sollten, falls das Tag und die Scriptbezeichnung richtig sind die Hitregistrations funktionieren.

--- Test_Mad_Flashlight_and_Sound/Assets/Look.cs
+++ Test_Mad_Flashlight_and_Sound/Assets/Look.cs
@@ -8,8 +8,13 @@ public class Look : MonoBehaviour {
 
     private float yaw = 0.0f;
     private float pitch = 0.0f;
+    
+         //Diese Optionen waren für das Umgucken selbst gedacht, da dies über den First Person Controller geht nicht weiter zu beachten.*/
+
 
     public Light flashlight;
+    Move[] targets;
+    private int count = 0;
 
 
 	// Use this for initialization
@@ -20,16 +25,19 @@ public class Look : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if (Input.GetMouseButtonDown(0))
-        {
+        {   // aktiviert, bzw deaktiviert die Taschenlampe per linksklick.
             flashlight.enabled = !flashlight.enabled;
         }
+        
 
         yaw += speedH * Input.GetAxis("Mouse X");
         pitch -= speedV * Input.GetAxis("Mouse Y");
+
         Vector3 v = new Vector3(pitch, yaw, 0.0f);
         transform.eulerAngles = v;
-
-
         
-	}
+
+
+
+    }
 }
